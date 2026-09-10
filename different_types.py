554a56b3faaa5f4e2b0b332260c770bb2d565613
Bair Ch. <360583r@gmail.com>
2026-09-10 20:31:56 +0800

    Типы данных

---
+++ different_types.py
@@ -0,0 +1,13 @@
+x = 7294
+y = 7.02
+z = "Bair"
+is_study = True
+is_rich = None
+
+print(x, "Thats a int")
+print(y, "Thats a float")
+print(z, "Thats a str")
+print(is_study, "Thats a bool")
+print(is_rich, "Thats a NoneType")
+
+
